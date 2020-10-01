53f6a169a4a8708469ed3071908946bd191d193a
Yan Qiufang <yanqf@outlook.com>
2020-10-01 23:26:35 +0800

    update shell scripts;

--- sugon/install.sh
+++ sugon/install.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 #
-#
-#
+# Python 3.6
+# Pip 3
 #
 
+echo "Begin at $(date "+%Y-%m-%d %H:%M:%S")"
 
+CUR_DIR=$(pwd)
+WORK_DIR=$(cd "$(dirname "$0")"; pwd)
+cd ${WORK_DIR}
+cd ..
+
+yum makecache
 ## RPMS
 
 
-## Python36 pip
+## pip
 
 
 ## Entry
 python3 setup.py install
 
 
+cd ${CUR_DIR}
--- sugon/uninstall.sh
+++ sugon/uninstall.sh
@@ -5,11 +5,7 @@
 #
 
 ## delete directory / file
-RM_LIST[0]="/var/lib/cobbler"
-RM_LIST[1]="/etc/cobbler"
-RM_LIST[2]="/var/www/cobbler"
-RM_LIST[3]="/var/log/cobbler"
-for rmFile in ${RM_LIST[*]} ; do
-    rm -rf ${rmFile}
-done
-
+rm -rf /var/lib/cobbler
+rm -rf /etc/cobbler
+rm -rf /var/www/cobbler
+rm -rf /var/log/cobbler
