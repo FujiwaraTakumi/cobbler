599f100c9cdce3c62a1b82f397ce5c90dd82427f
Yan Qiufang <yanqf@outlook.com>
2020-11-19 12:12:27 +0800

    test tftp server status;

---
+++ sugon/tftp_test.sh
@@ -0,0 +1,13 @@
+#
+# TFTP SERVER TEST
+#
+# tftp 172.16.0.2
+
+get uefi/bootx64.efi
+get uefi/grub.cfg
+get grub2/lx/core.efi
+get grub2/lx/grub.cfg
+get grub2/ft/grubaa64.efi
+get grub2/ft/grub.cfg
+get pxelinux.0
+
